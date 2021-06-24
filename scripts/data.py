0fa8b5c773c5ad5753846fb2c00d5ee92e209378
ngrtp <ngravindra@gmail.com>
2021-06-24 14:01:33 -0700

    started some metadata_issue4 things

--- scripts/data.py
+++ scripts/data.py
@@ -48,5 +48,26 @@ def get_train_test():
     X_test, y_test = pad_align_transform(data_test)
     return {'X_train':X_train, 'y_train':y_train, 'X_test':X_test, 'y_test':y_test}
 
-def load_actigraphy_metadata(fname='/home/ngr/gdrive/wearables/data/MOD_Data_2021.csv'):
+def load_actigraphy_metadata(fname='/home/ngr/gdrive/wearables/data/MOD_1000_Woman_Activity_Data/MOD_Data_2021.csv'):
     return pd.read_csv(fname, index_col=0)
+
+
+def pad_align_transform_yID(data):
+    '''Treat y as identifier to map with dictionary version of metadata outcomes
+    
+    '''
+    X = np.empty((len(data.keys()), 24*60))
+    y = np.empty((len(data.keys()),))
+    for i, (k, v) in enumerate(data.items()):
+        ts, act = v[0], v[1]
+        first_hour, first_min = ts[0].hour, ts[0].minute
+        zeros2pad = np.zeros((first_hour*60 + first_min + 1))
+        act = np.concatenate((zeros2pad, [float(i) for i in act[:24*60 - zeros2pad.shape[0]]]))
+        if act.shape[0] < 24*60:
+            act = np.concatenate((act, np.zeros((24*60-act.shape[0], ))))
+
+        # add log-pseudocount
+        act = np.log(act + 1)
+        X[i, :] = act
+        y[i] = k
+    return X, y
