021c6d569ac5cb7479401cd7799e595b6dd6b008
Neal Giri Ravindra <ngrav@anes-nalab4.stanford.edu>
2022-01-12 13:24:36 -0800

    interpretability and hunting for why model ascribes differences

--- scripts/eval_.py
+++ scripts/eval_.py
@@ -206,6 +206,374 @@ def summarize_long_table(results_longtab, metrics=['MAE', 'Rho'], group='Model',
         summary.to_csv(out_file)
     return summary
             
+# ~20min for inference 
+def merge_out2md(md, bst_trainerfp, bst_modelfp, return_embeds=True, out_file=None, verbose=False):
+    def loadpkl(file):
+        with open(file, 'rb') as f:
+            data = pickle.load(f)
+            f.close()
+        return data
+    trainer = loadpkl(bst_trainerfp)
+    if verbose:
+        total_t = time.time()
+    dt = pd.DataFrame()
+    if return_embeds:
+        embeds = pd.DataFrame()
+    for split in ['train', 'val', 'test']:
+        if verbose:
+            tic = time.time()
+            print('Starting inference for {} set...'.format(split))
+
+        evaluation = weareval.eval_trained(trainer, split=split,
+                                           modelpkl=bst_modelfp,
+                                           two_outputs=True)
+        dt = dt.append(pd.DataFrame({
+            'y':evaluation.y.numpy(), 'yhat':evaluation.yhat.numpy(), 
+            'split':[split]*evaluation.y.shape[0],
+            'error':(evaluation.yhat - evaluation.y).numpy()
+        }, index=evaluation.id))
+        if return_embeds:
+            embeds = embeds.append(pd.DataFrame(evaluation.out2.numpy(), index=evaluation.id))
+        if verbose:
+            print('  inference for {} set done in {:.0f}-s\t{:.2f}-min elapsed'.format(split, time.time()-tic, (time.time()-total_t)/60))
+    md = md.merge(dt, left_index=True, right_index=True, how='left')
+    if out_file is not None:
+        md.to_csv(out_file)
+    if return_embeds:
+        if out_file is not None:
+            embeds.to_csv(os.path.join(os.path.split(out_file)[0], 'embeds_v52.csv'))
+        return md, embeds
+    else:
+        return md
+    
+# PathExplain
+import functools
+import operator
+import torch
+from torch.autograd import grad
+import numpy as np
+from tqdm import *
+
+def gather_nd(params, indices):
+    """
+    Args:
+        params: Tensor to index
+        indices: k-dimension tensor of integers. 
+    Returns:
+        output: 1-dimensional tensor of elements of ``params``, where
+            output[i] = params[i][indices[i]]
+            
+            params   indices   output
+            1 2       1 1       4
+            3 4       2 0 ----> 5
+            5 6       0 0       1
+    """
+    max_value = functools.reduce(operator.mul, list(params.size())) - 1
+    indices = indices.t().long()
+    ndim = indices.size(0)
+    idx = torch.zeros_like(indices[0]).long()
+    m = 1
+
+    for i in range(ndim)[::-1]:
+        idx += indices[i]*m
+        m *= params.size(i)
+
+    idx[idx < 0] = 0
+    idx[idx > max_value] = 0
+    return torch.take(params, idx)
+
+class PathExplainerTorch(object):
+    def __init__(self, model):
+        self.model = model
+        return
+    
+    def _get_ref_tensor(self,baseline,batch_size,num_samples):
+        number_to_draw = num_samples * batch_size
+        replace = baseline.shape[0] < number_to_draw
+        sample_indices = np.random.choice(baseline.shape[0],
+                                          size=number_to_draw,
+                                          replace=replace)
+        ref_tensor = baseline[sample_indices,:]
+        
+        return ref_tensor
+
+    def _get_samples_input(self, input_tensor, baseline, 
+                           num_samples, use_expectation):
+        '''
+        calculate interpolation points
+        Args:
+            input_tensor: Tensor of shape (batch, ...), where ... indicates
+                          the input dimensions. 
+            reference_tensor: A tensor of shape (batch, k, ...) where ... 
+                indicates dimensions, and k represents the number of background 
+                reference samples to draw per input in the batch.
+        Returns: 
+            samples_input: A tensor of shape (batch, k, ...) with the 
+                interpolated points between input and ref.
+            samples_delta: A tensor of shape (batch, 1, ...) with the
+                difference between input and reference for each sample
+        '''
+        input_dims = list(input_tensor.size())[1:]
+        num_input_dims = len(input_dims)
+        batch_size = input_tensor.size()[0]
+        
+        if use_expectation:
+            reference_tensor = self._get_ref_tensor(baseline,batch_size,num_samples)
+            shape = reference_tensor.shape
+            reference_tensor = reference_tensor.view(
+                    batch_size, 
+                    num_samples, 
+                    *(shape[1:]))
+            
+            # Grab a [batch_size, k]-sized interpolation sample
+            t_tensor = torch.FloatTensor(batch_size, num_samples).uniform_(0,1).to(reference_tensor.device)
+            shape = [batch_size, num_samples] + [1] * num_input_dims
+            interp_coef = t_tensor.view(*shape)
+
+            # Evaluate the end points
+            end_point_ref = (1.0 - interp_coef) * reference_tensor
+
+            input_expand_mult = input_tensor.unsqueeze(1)
+            end_point_input = interp_coef * input_expand_mult
+
+            # Affine Combine
+            samples_input = end_point_input + end_point_ref
+            
+        else:
+            batch_size = input_tensor.size()[0]
+            input_expand = input_tensor.unsqueeze(1)
+            reps = np.ones(len(baseline.shape)).astype(int)
+            reps[0] = batch_size
+            reference_tensor = baseline.repeat(list(reps)).unsqueeze(1)
+#             reference_tensor = torch.as_tensor(sampled_baseline).unsqueeze(1).to(baseline.device)
+            scaled_inputs = [reference_tensor + (float(i)/(num_samples-1))*(input_expand - reference_tensor) \
+                             for i in range(0,num_samples)]
+            samples_input = torch.cat(scaled_inputs,dim=1)
+        
+        samples_delta = self._get_samples_delta(input_tensor, reference_tensor)
+        samples_delta = samples_delta.to(samples_input.device)
+        
+        return samples_input, samples_delta
+    
+    def _get_samples_delta(self, input_tensor, reference_tensor):
+        input_expand_mult = input_tensor.unsqueeze(1)
+        sd = input_expand_mult - reference_tensor
+        return sd
+    
+    def _get_grads(self, samples_input, output_indices=None):
+
+        grad_tensor = torch.zeros(samples_input.shape).float().to(samples_input.device)
+        
+        k_ = samples_input.shape[1]
+
+        for i in range(k_):
+            particular_slice = samples_input[:,i]
+            batch_output = self.model(particular_slice)
+            # should check that users pass in sparse labels
+            # Only look at the user-specified label
+            if batch_output.size(1) > 1:
+                sample_indices = torch.arange(0,batch_output.size(0)).to(samples_input.device)
+                indices_tensor = torch.cat([
+                        sample_indices.unsqueeze(1), 
+                        output_indices.unsqueeze(1)], dim=1)
+                batch_output = gather_nd(batch_output, indices_tensor)
+
+            model_grads = grad(
+                    outputs=batch_output,
+                    inputs=particular_slice,
+                    grad_outputs=torch.ones_like(batch_output).to(samples_input.device),
+                    create_graph=True)
+            grad_tensor[:,i,:] = model_grads[0]
+        return grad_tensor
+           
+    def attributions(self, input_tensor, baseline,
+                     num_samples = 50, use_expectation=True, 
+                     output_indices=None):
+        """
+        Calculate either Expected or Integrated Gradients approximation of 
+        Aumann-Shapley values for the sample ``input_tensor``.
+        Args:
+            model (torch.nn.Module): Pytorch neural network model for which the
+                output should be explained.
+            input_tensor (torch.Tensor): Pytorch tensor representing the input
+                to be explained.
+            baseline (torch.Tensor): Pytorch tensor representing the baseline.
+                If use_expectation is true, then baseline should be shape
+                (num_refs, ...) where ... indicates the dimensionality
+                of the input. Otherwise, baseline should be shape (1, ...).
+            output_indices (optional, default=None): For multi-class prediction
+        """
+        equal_dims = baseline.shape[1:] == input_tensor.shape[1:]
+        almost_equal_dims = baseline.shape == input_tensor.shape[1:]
+        
+        dev = input_tensor.device
+        baseline = baseline.to(dev)
+        
+        input_tensor.requires_grad_ = True
+        
+        if use_expectation and not equal_dims:
+            raise ValueError('baseline should be shape (num_refs, ...) \
+                              where ... indicates the dimensionality   \
+                              of the input')
+            
+        if not use_expectation and baseline.shape[0] != 1:
+            if almost_equal_dims:
+                baseline = baseline.unsqueeze(0)
+            else:
+                raise ValueError('baseline should be shape (...)           \
+                                  where ... indicates the dimensionality   \
+                                  of the input')
+        
+        samples_input, samples_delta = self._get_samples_input(input_tensor, baseline,
+                                                               num_samples, use_expectation)
+        grad_tensor = self._get_grads(samples_input, output_indices)
+        mult_grads = samples_delta * grad_tensor
+        attributions = mult_grads.mean(1)
+        
+        return attributions
+    
+    def interactions(self, input_tensor, baseline,
+                     num_samples=50, use_expectation=True,
+                     output_indices=None, interaction_index=None,
+                     verbose=True):
+        """
+            input_tensor (torch.Tensor): Pytorch tensor representing the input
+                to be explained.
+            baseline (torch.Tensor): Pytorch tensor representing the baseline.
+                If use_expectation is true, then baseline should be shape
+                (num_refs, ...) where ... indicates the dimensionality
+                of the input. Otherwise, baseline should be shape (1, ...)
+            num_samples: The number of samples to use when computing the
+                         expectation or integral.
+            use_expectation: If True, this samples baselines and interpolation
+                             constants uniformly at random (expected gradients).
+                             If False, then this assumes num_refs=1 in which
+                             case it uses the same baseline for all inputs,
+                             or num_refs=batch_size, in which case it uses
+                             baseline[i] for inputs[i] and takes 100 linearly spaced
+                             points between baseline and input (integrated gradients).
+            output_indices:  If this is None, then this function returns the
+                             attributions for each output class. This is rarely
+                             what you want for classification tasks. Pass an
+                             integer tensor of shape [batch_size] to
+                             index the output output_indices[i] for
+                             the input inputs[i].
+            interaction_index: Either None or an index into the input. If the latter,
+                               will compute the interactions with respect to that
+                               feature. This parameter should index into a batch
+                               of inputs as inputs[(slice(None) + interaction_index)].
+                               For example, if you had images of shape (32, 32, 3)
+                               and you wanted interactions with respect
+                               to pixel (i, j, c), you should pass
+                               interaction_index=[i, j, c].
+        
+        """
+        
+        if len(input_tensor.shape) != 2:
+            raise ValueError('PyTorch Explainer only supports ' + \
+                             'interaction for 2D input tensors!')
+        
+        equal_dims = baseline.shape[1:] == input_tensor.shape[1:]
+        almost_equal_dims = baseline.shape == input_tensor.shape[1:]
+        
+        if use_expectation and not equal_dims:
+            raise ValueError('baseline should be shape (num_refs, ...) \
+                              where ... indicates the dimensionality   \
+                              of the input')
+            
+        if not use_expectation and baseline.shape[0] != 1:
+            if almost_equal_dims:
+                baseline = baseline.unsqueeze(0)
+            else:
+                raise ValueError('baseline should be shape (...)           \
+                                  where ... indicates the dimensionality   \
+                                  of the input')
+        
+        inner_loop_nsamples = int(round(np.sqrt(num_samples)))
+        
+        samples_input, samples_delta = self._get_samples_input(input_tensor, baseline,
+                                                               inner_loop_nsamples, use_expectation)
+        
+        if interaction_index is not None:
+            interaction_mult_tensor = torch.zeros([input_tensor.size(0), samples_input.size(1), input_tensor.size(1)])
+        else:
+            interaction_mult_tensor = torch.zeros([input_tensor.size(0), samples_input.size(1), 
+                                                   input_tensor.size(1), input_tensor.size(1)])
+            
+        ig_tensor = torch.zeros(samples_input.shape).float()
+        
+        loop_num = inner_loop_nsamples
+        
+        if verbose:
+            iterable = tqdm(range(loop_num))
+        else:
+            iterable = range(loop_num)
+        
+        for i in iterable:
+            
+            particular_slice = samples_input[:,i]
+            ig_tensor[:,i,:] = self.attributions(particular_slice, baseline,
+                                                 num_samples=inner_loop_nsamples, use_expectation=use_expectation,
+                                                 output_indices=output_indices)
+            
+            if interaction_index is not None:
+                second_grads = grad(
+                        outputs=ig_tensor[:,i,interaction_index],
+                        inputs=particular_slice,
+                        grad_outputs=torch.ones_like(ig_tensor[:,i,interaction_index]),
+                        create_graph=True)[0].detach()
+                interaction_mult_tensor[:,i,:] = second_grads
+
+            else:
+                for feature in range(input_tensor.size(1)):
+                    second_grads = grad(
+                        outputs=ig_tensor[:,i,feature],
+                        inputs=particular_slice,
+                        grad_outputs=torch.ones_like(ig_tensor[:,i,feature]),
+                        create_graph=True)[0].detach()
+                    interaction_mult_tensor[:,i,feature,:] = second_grads
+
+        interaction_mult_tensor = interaction_mult_tensor.to(samples_delta.device)
+        if interaction_index is not None:
+            interaction_tensor = interaction_mult_tensor * samples_delta
+        else:
+            interaction_tensor = interaction_mult_tensor * samples_delta.unsqueeze(2)
+        interactions = interaction_tensor.mean(1)
+        
+        return interactions
+    
+    
+def featattr_peruid(X, md, uids, explainer, trainer):
+    '''
+    Returns:
+      df (pd.DataFrame)
+      uids (dict): 'groupname': list of unique ids
+    '''
+    total_t = time.time()
+    df = pd.DataFrame()
+    counter = 0
+    for i, g in enumerate(uids.keys()):
+        for uid in uids[g]:
+            idx = np.where(md.index==uid)[0].item()
+            x = X[idx, :, :].unsqueeze(0)
+            x.requires_grad = True
+            print('starting idx: {}, unique id: {}\{:.2f}-min elapsed'.format(idx, uid, (time.time()-total_t)/60))
+            if counter==0: # only need to call once
+                x_baseline = torch.zeros_like(x)
+                x_baseline.requires_grad = True
+            attr = explainer.attributions(input_tensor=x,
+                                          baseline=x_baseline,
+                                          num_samples=20,
+                                          use_expectation=True,
+                                          output_indices=0)
+            dt = pd.DataFrame(attr[0, 0, :].detach().numpy(), columns=['attr'])
+            dt['uid'] = uid
+            dt['sleep'] = trainer.data.data['data'][uid]['sleep'][:-1].to_numpy()
+            dt['group'] = g
+            df = df.append(dt)
+            counter += 1
+    return df
+
 
 # if __name__ == '__main__':
 #     mode = 'weighted_AU-PRC'
